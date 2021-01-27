350bdf6e7ee39908c731601131fd4d15caed1eb9
Kornel Mrozowski <kpmrozowski@gmail.com>
2021-01-27 04:14:02 +0100

    AI's unbeatatness improved

--- TicTacToeMinMax/main.cpp
+++ TicTacToeMinMax/main.cpp
@@ -1,55 +1,44 @@
 #include <iostream>
 #include <vector>
 #include <array>
-#include <exception>
+#include <random>
 
 struct move {
 	int r, c, score;
 };
 
 struct TicTacToe {
-	char player{}, computer{};
-
-	//std::vector<std::vector<char>> grid;
-	//		\/ \/ \/		// slower
-	char grid[3][3]{};
 
 	TicTacToe() {
+		grid = std::vector<std::vector<int>>(3, std::vector<int>(3, -1));
+	}
+	~TicTacToe() {}
 
-		for(int i = 0; i < 3; i++)
-			for(int j = 0; j < 3; j++)
-				grid[i][j] = ' ';
-		//		\/ \/ \/		// slower
-		//grid = std::vector<std::vector<char>>(3, std::vector<char>(3, ' '));
+	int player{}, computer{};
+	std::vector<std::vector<int>> grid;
+
+	void makeRandomMove(int who) {
+		std::mt19937 mt(std::random_device{}());
+		std::uniform_int_distribution<int> dist(0, 2);
+		grid[dist(mt)][dist(mt)] = who;
 	}
 
 	bool win() {
 
-		//	Winning configurations:
-		int win_statess[8][3] = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
-		//		\/ \/ \/		//	hugely slower!
-		//std::vector<std::vector<int>> win_states = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
+		std::vector<std::vector<int>> win_states = {{0, 1, 2}, {3, 4, 5}, {6, 7, 8}, {0, 3, 6}, {1, 4, 7}, {2, 5, 8}, {0, 4, 8}, {2, 4, 6}};
 
-		for(int i = 0; i < 8; i++) {
-			int first_r = win_statess[i][0] / 3;
-			int first_c = win_statess[i][0] % 3;
-			//		\/ \/ \/		//	hugely slower!
-			//for(auto i : win_states) {
-			//	int first_r = i[0] / 3;
-			//	int first_c = i[0] % 3;
+			for(auto i : win_states) {
+				int first_r = i[0] / 3;
+				int first_c = i[0] % 3;
 
 			bool win = true; // Let's assume that there's win, and if there is a contradiction (dowod nie w prost), change it to flalse
 
-			for(int j = 0; j < 3; j++) {
-				int r = win_statess[i][j] / 3;
-				int c = win_statess[i][j] % 3;
-				//		\/ \/ \/		// hugely slower!
-				//for(auto j : i) {
-				//int r = j / 3;
-				//int c = j % 3;
+				for(auto j : i) {
+				int r = j / 3;
+				int c = j % 3;
 
 				//	If the first element in a configuration is empty or if elements in a configuration are not the same, than there's no win:
-				if(grid[first_r][first_c] == ' ' || grid[first_r][first_c] != grid[r][c]) {
+				if(grid[first_r][first_c] == -1 || grid[first_r][first_c] != grid[r][c]) {
 					win = false;
 				}
 			}
@@ -63,21 +52,13 @@ struct TicTacToe {
 		if(win())
 			return false;
 
-		for(int i = 0; i < 3; i++) {
-			for(int j = 0; j < 3; j++) {
-				if(grid[i][j] == ' ') {
-					return false;
+			for (auto i : grid) {
+				for (auto j :  i) {
+					if (j == -1) {
+						return false;
+					}
 				}
 			}
-		}
-		//		\/ \/ \/		// slower
-		//	for (auto i : grid) {
-		//		for (auto j :  i) {
-		//			if (j == ' ') {
-		//				return false;
-		//			}
-		//		}
-		//	}
 
 		return true;
 	}
@@ -88,22 +69,18 @@ struct TicTacToe {
 			int cell;
 			try {
 				std::cin >> cell;
-				//std::cout << cell --> display wha is in the cell
 				// cin.fail() --> fail if type is not correct
-				// static_cast<int>(cell) != a --> check if a is integer or not
 				if(std::cin.fail() || (static_cast<int>(cell) != cell)) {
 					std::cout << "\nWrong input. Please enter one integer\n";
 					std::cin.clear();
 					std::cin.ignore(256, '\n'); // ignore the line change
-														 //std::cin >> cell;
 				}
 			} catch(std::exception& e) {
 				std::cout << "\nYou entered: " << e.what() << "\nAnd you were ment to enter a number from range 1 to 9\n";
-				//player_move();
 			}
 			int r = (cell - 1) / 3;
 			int c = (cell - 1) % 3;
-			if(cell >= 1 && cell <= 9 && grid[r][c] == ' ') {
+			if(cell >= 1 && cell <= 9 && grid[r][c] == -1) {
 				grid[r][c] = player;
 				break;
 			} else {
@@ -118,9 +95,7 @@ struct TicTacToe {
 	}
 
 	// First we check if the player is maximizing_player;
-	//	If you are the maximizing_player then we will try
-	//		to maximize your score, but if y're not then
-	//		we want to harm you as much as posible:
+	//	If you are the maximizing_player then we will try to maximize your score, but if y're not then we want to harm you as much as posible:
 	move minimax(bool maximizing_player = true) {
 		move best_move;
 		if(win()) {
@@ -143,14 +118,13 @@ struct TicTacToe {
 
 		for(int i = 0; i < 3; i++) {
 			for(int j = 0; j < 3; j++) {
-				if(grid[i][j] == ' ') {
-					//	Ai tries to maximize it's score and player
-					//		is tring to minimize AI's score
+				if(grid[i][j] == -1) {
+					//	Ai tries to maximize it's score and player is tring to minimize AI's score
 					grid[i][j] = maximizing_player ? computer : player;
+					//print();	uncomment to see AI's analizes
 					move board_state = minimax(!maximizing_player);
 					if(maximizing_player) {
-						//	if the move is better than the move we'd
-						//		already found or if we haven't found it yet:
+						//	if the move is better than the move we'd already found or if we haven't found it yet:
 						if(board_state.score > best_move.score) {
 							// best move is the found one:
 							best_move.score = board_state.score;
@@ -165,7 +139,7 @@ struct TicTacToe {
 							best_move.c = j;
 						}
 					}
-					grid[i][j] = ' '; // reset analised pole
+					grid[i][j] = -1; // reset analised pole
 				}
 			}
 		}
@@ -173,28 +147,35 @@ struct TicTacToe {
 	}
 
 	void play() {
+		char imput{};
+		int turnNr{0};
+
 		while(true) {
 			std::cout << "Which symbol (X or O, X goes first)? ";
-			std::cin >> player;
-			if(player == 'X' || player == 'O' || player == 'x' || player == 'o') {
+			std::cin >> imput;
+			if(imput == 'X' || imput == 'O' || imput == 'x' || imput == 'o') {
 				break;
 			}
 		}
 
-		if(player == 'x')
-			player = 'X';
-		if(player == 'o')
-			player = 'O';
-		computer = player == 'X' ? 'O' : 'X';
+		if(imput == 'x' || imput == 'X')
+			player = 8;
+		if(imput == 'o' || imput == 'O')
+			player = 0;
+		computer = player == 8 ? 0 : 8;
 
-		if(player == 'O') {
-			computer_move();
+		if(player == 0) {
+			std::cout << "Computer is making a move...\n";
+			makeRandomMove(computer);
+			turnNr++;
 		}
 
 		print();
 
+		// main game loop:
 		while(true) {
 			player_move();
+			turnNr++;
 			print();
 
 			if(win()) {
@@ -206,7 +187,43 @@ struct TicTacToe {
 			}
 
 			std::cout << "Computer is making a move...\n";
-			computer_move();
+			if(turnNr > 1) {
+				computer_move();
+			} else {
+				// Find player's first move:
+				move playerFirstMove{};
+				for(int i = 0; i < 3; i++)
+					for(int j = 0; j < 3; j++)
+						if(grid[i][j] == player) {
+							playerFirstMove.r = i;
+							playerFirstMove.c = j;
+						}
+
+				//	In the AI's first turn: place "O" on the oposite side of "X"
+				std::mt19937 mt(std::random_device{}());
+				std::uniform_int_distribution<int> dist(0, 1);
+				switch(playerFirstMove.r * 3 + playerFirstMove.c) {
+					case 1:
+						grid[2][1] = computer;
+						break;
+					case 3:
+						grid[1][2] = computer;
+						break;
+					case 4:
+						grid[2 * dist(mt)][2 * dist(mt)] = computer;
+						break;
+					case 5:
+						grid[1][0] = computer;
+						break;
+					case 7:
+						grid[0][1] = computer;
+						break;
+					default:
+						grid[1][1] = computer;
+						break;
+				};
+			}
+			
 			print();
 
 			if(win()) {
@@ -232,10 +249,12 @@ struct TicTacToe {
 					std::cout << "|";
 				std::cout << ' ';
 
-				if(grid[i][j] == ' ') {
+				if(grid[i][j] == -1) {
 					std::cout << 3 * i + j + 1;
+				} else if(grid[i][j] == 0) {
+					std::cout << 'O';
 				} else {
-					std::cout << grid[i][j];
+					std::cout << 'X';				
 				}
 				std::cout << ' ';
 			}
